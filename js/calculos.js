d07f63d4466b70923f35a8fcd817eb6482ac199d
vitipe <victorcasbas@gmail.com>
2021-01-04 12:57:29 -0300

    Ideas generales
    Ideas generales para pensar como tendría que hacer el simón dice de cero

--- js/calculos.js
+++ js/calculos.js
@@ -1,2 +1,18 @@
 //TO-DO
 //Crear una base de 4 botones en el centro con 4 colores diferentes. A eso agregar un boton "jugar".
+
+/*
+En base a la interfaz de 4 botones.
+
+Tendria que haber un boton iniciar. Al hacer click, comienza el juego.
+
+Por cada boton hay un color y un ID (1,2,3,4), al hacer click en "jugar" tendria que tomar uno de los botones random y cambiarle el color.
+Al mismo tiempo, crear un array que contenga el/los numeros y que haga un random y en base a ese random ilumine el boton.
+Si es correcto, sumar a ese array otro random 1,2,3,4 y que ilumne los dos seguidos del otro. Ademas crear un contador de "correctos"
+Si es incorrecto, msj de que el usuario le erró y mostrar cuantas correctas llegó a hacer.
+Agregar set.timeout para que tarde entre mostrar uno y otro. ver como hacer para que el boton quede "encendido" medio segundo o algo asi.
+eventListener para escuchar la respuesta del usuario y que la espere la funcion?
+
+
+
+*/
