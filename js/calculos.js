5d06fb394bfeec4bf47472d064a527f61c126c2e
vitipe <victorcasbas@gmail.com>
2021-01-22 12:20:36 -0300

    Reemplaza "div" por "cuadro" en todo el código
    Porque con "div" no se entendía bien a qué se refería

--- js/calculos.js
+++ js/calculos.js
@@ -1,12 +1,18 @@
 /*
     TO-DO
 
-1. Mensaje que diga que vas mejorando si aumentaste los puntos con respecto a la ultima jugada?
+1. Escribir las pruebas. (capaz agregar que se ingrese un nombre de jugador y pruebas en base a eso)
+1/2. Mensaje que diga que vas mejorando si aumentaste los puntos con respecto a la ultima jugada?
 2. Tirare el pull a Fabri a ver que te dice.
 3. Armar un leaderboard
 4. Ver de poner las variables globales adentro de algunas funciones  (que pasa si dos functions la comparten?)
 5. Poner todas las functions en calculos y armar un .js nuevo con lo que es manejo del DOM? es asi?
 6. Armar Bootstrap, crear un branch nuevo o usar este mismo y dejar el juego andando bien en el master.
+
+Cosas que saque de ver la clase 10:
+1. Que haya un texto que diga "juega la maquina" en vez del borde negro que puse yo.
+2.
+3.
 
 
 Modos de juego a ver si agrego?:
@@ -17,24 +23,65 @@ Modos de juego a ver si agrego?:
 
 */
 
-let jugadaUsuario = []; //ver de mandarlo adentro de alguna function, es la unica variable global que me quedó
+let jugadaUsuario = [];
 let jugadaComputadora = crearJugadaComputadora();
 bloquearClickUsuario();
 
+document.querySelector("#boton-jugar").onclick = function() {
+    juegaComputadora();
+    ocultarBotonJugar();
+};
+
+document.querySelector('#cuadro-1').onclick = function() {
+    colorearClicksUsuario('#cuadro-1');
+    jugadaUsuario.push(1);
+    chequearResultadoJugada(jugadaUsuario, jugadaComputadora);
+}
+
+document.querySelector('#cuadro-2').onclick = function() {
+    colorearClicksUsuario('#cuadro-2');
+    jugadaUsuario.push(2)
+    chequearResultadoJugada(jugadaUsuario, jugadaComputadora);
+}
+
+document.querySelector('#cuadro-3').onclick = function() {
+    colorearClicksUsuario('#cuadro-3');
+    jugadaUsuario.push(3)
+    chequearResultadoJugada(jugadaUsuario, jugadaComputadora);
+}
+
+document.querySelector('#cuadro-4').onclick = function() {
+    colorearClicksUsuario('#cuadro-4');
+    jugadaUsuario.push(4)
+    chequearResultadoJugada(jugadaUsuario, jugadaComputadora);
+}
+
+document.querySelector('#boton-continuar-juego').onclick = function() {
+    ocultarErrorJugada();
+    resetearJugadaUsuario(jugadaUsuario);
+    juegaComputadora();
+    return false;
+}
+
+document.querySelector('#boton-reinicio-juego').onclick = function() {
+    document.querySelector('#form-juego').reset();
+}
+
+
 function obtenerNumeroRandom() {
-    let numeroRandom = Math.floor(Math.random() * 4 + 1); //El valor tiene que ser entre 1 y 4
+    let CANTIDAD_BOTONES = 4
+    let numeroRandom = Math.floor(Math.random() * CANTIDAD_BOTONES + 1);
     return numeroRandom;
 }
 
 function colorearBotones(jugadaComputadora) {
     let DELAY_COLOR_JUGADA = 500;
     let DELAY_COLOR_NORMAL = 1000;
-
     bloquearClickUsuario();
 
     for (let i = 0; i < jugadaComputadora.length; i++) {
 
-        let $botonEnJuego = document.querySelector(`#div-${jugadaComputadora[i]}`);
+        let $botonEnJuego = document.querySelector(`#cuadro-${jugadaComputadora[i]}`);
 
         setTimeout(function() {
             $botonEnJuego.className = 'en-juego';
@@ -60,7 +107,6 @@ function crearJugadaComputadora() {
     for (let i = 0; i < turnoUsuario; i++) { //probar usar ForEach?
         jugadaComputadora.push(obtenerNumeroRandom());
     }
-
     return jugadaComputadora;
 }
 
@@ -88,7 +134,6 @@ function contadorPuntosUsuario() { //esto y lo de arriba no los puedo hacer en u
     for (let i = 0; i < jugadaComputadora.length; i++) {
         puntosUsuario++
     }
-
     return puntosUsuario - 1;
 }
 
@@ -115,35 +160,32 @@ function ocultarBotonJugar() {
 }
 
 function bloquearClickUsuario() {
-    document.querySelector('#div-1').style.pointerEvents = "none";
-    document.querySelector('#div-2').style.pointerEvents = "none";
-    document.querySelector('#div-3').style.pointerEvents = "none";
-    document.querySelector('#div-4').style.pointerEvents = "none";
-
+    document.querySelector('#cuadro-1').style.pointerEvents = "none";
+    document.querySelector('#cuadro-2').style.pointerEvents = "none";
+    document.querySelector('#cuadro-3').style.pointerEvents = "none";
+    document.querySelector('#cuadro-4').style.pointerEvents = "none";
     document.querySelector('#tablero-juego').style.borderStyle = "solid";
     document.querySelector('#tablero-juego').style.borderColor = "black";
     document.querySelector('#tablero-juego').style.borderRadius = "2px";
-
 }
 
 function desbloquearClickUsuario() {
-    document.querySelector('#div-1').style.pointerEvents = "auto";
-    document.querySelector('#div-2').style.pointerEvents = "auto";
-    document.querySelector('#div-3').style.pointerEvents = "auto";
-    document.querySelector('#div-4').style.pointerEvents = "auto";
-
+    document.querySelector('#cuadro-1').style.pointerEvents = "auto";
+    document.querySelector('#cuadro-2').style.pointerEvents = "auto";
+    document.querySelector('#cuadro-3').style.pointerEvents = "auto";
+    document.querySelector('#cuadro-4').style.pointerEvents = "auto";
     document.querySelector('#tablero-juego').style.borderStyle = "";
     document.querySelector('#tablero-juego').style.borderColor = "";
     document.querySelector('#tablero-juego').style.borderRadius = "";
 }
 
-function colorearClicksUsuario(divClickeado) {
+function colorearClicksUsuario(cuadroClickeado) {
     setTimeout(function() {
-        document.querySelector(divClickeado).className = 'clickeado';
+        document.querySelector(cuadroClickeado).className = 'clickeado';
     }, 0);
 
     setTimeout(function() {
-        document.querySelector(divClickeado).className = '';
+        document.querySelector(cuadroClickeado).className = '';
     }, 75);
 }
 
@@ -168,43 +210,3 @@ function chequearResultadoJugada(jugadaUsuario, jugadaComputadora) { //Subdividi
     }
 }
 
-document.querySelector("#boton-jugar").onclick = function() {
-    juegaComputadora();
-    ocultarBotonJugar();
-};
-
-document.querySelector('#div-1').onclick = function() {
-    colorearClicksUsuario('#div-1');
-    jugadaUsuario.push(1);
-    chequearResultadoJugada(jugadaUsuario, jugadaComputadora);
-}
-
-document.querySelector('#div-2').onclick = function() {
-    colorearClicksUsuario('#div-2');
-    jugadaUsuario.push(2)
-    chequearResultadoJugada(jugadaUsuario, jugadaComputadora);
-}
-
-document.querySelector('#div-3').onclick = function() {
-    colorearClicksUsuario('#div-3');
-    jugadaUsuario.push(3)
-    chequearResultadoJugada(jugadaUsuario, jugadaComputadora);
-}
-
-document.querySelector('#div-4').onclick = function() {
-    colorearClicksUsuario('#div-4');
-    jugadaUsuario.push(4)
-    chequearResultadoJugada(jugadaUsuario, jugadaComputadora);
-}
-
-document.querySelector('#boton-continuar-juego').onclick = function() {
-    ocultarErrorJugada();
-    resetearJugadaUsuario(jugadaUsuario);
-    juegaComputadora();
-
-    return false;
-}
-
-document.querySelector('#boton-reinicio-juego').onclick = function() {
-    document.querySelector('#form-juego').reset();
-}
